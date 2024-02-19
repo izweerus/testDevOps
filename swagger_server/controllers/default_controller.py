d8a4e4d9e2f3a267b893fdceb21c339085df7e9e
Igor <izweerus1@gmail.com>
2024-02-19 21:11:48 +0100

    service added

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,23 +1,23 @@
 import connexion
 import six
 
+from swagger_server.service.student_service import *
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
 
 
-def add_student(body=None):  # noqa: E501
+def add_student(body=None): # noqa: E501
     """Add a new student
-
     Adds an item to the system # noqa: E501
-
     :param body: Student item to add
     :type body: dict | bytes
-
     :rtype: float
     """
     if connexion.request.is_json:
-        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        body = Student.from_dict(connexion.request.get_json()) # noqa: E501
+        return add(body)
+    return 500,'error'
+
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +30,10 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
+        return delete(body)
+    return 500, 'error'
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +46,7 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
+        return get_by_id(body)
+    return 500, 'error'
